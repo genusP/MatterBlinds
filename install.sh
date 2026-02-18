5f93e83900018a87ac697b7fe1a09f8a1d654a69
Gennady Pundikov <genusp@outlook.com>
2026-02-18 13:01:33 +0000

    setup Matter

--- install.sh
+++ install.sh
@@ -1,26 +1,8 @@
 #!/bin/bash
-set -e
+root_dir=$PWD
 
-# 1. Активируем IDF
-. $IDF_PATH/export.sh
+cd $ESP_MATTER_PATH
+source  ./install.sh 
+source ./export.sh
 
-# 2. Клонируем оболочку Matter (без сабмодулей пока что)
-if [ ! -d "esp-matter" ]; then
-    git clone --depth 1 https://github.com/espressif/esp-matter.git
-fi
-
-cd esp-matter
-
-# 3. Инициализируем только базовые сабмодули самого Matter
-git submodule update --init --depth 1
-
-# 4. Входим в недра ConnectedHomeIP и качаем только для ESP32 и Linux
-cd ./connectedhomeip/connectedhomeip
-./scripts/checkout_submodules.py --platform esp32 linux --shallow
-
-# 5. Возвращаемся и запускаем финальную установку инструментов
-cd ../..
-./install.sh
-
-cd ..
-echo "Установка по официальному методу завершена!"
+cd $root_dir
